53f609d420a7b7df3cc597692b79186efc55e352
Hassan Raza Bukhari <51364060+eruditehassan@users.noreply.github.com>
2019-06-15 23:51:55 +0500

    Create PersonInterface.java

---
+++ Interfaces/PersonInterface/PersonInterface.java
@@ -0,0 +1,6 @@
+public interface PersonInterface {
+    //Person Interface that will be further implemented in other class(es)
+    //Defining all the methods that will be implementd
+    int computeTotalWealth();
+    String getName();
+}
