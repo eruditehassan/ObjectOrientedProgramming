83a5357b286ecf59f1f981995b25467b1697fe5b
Hassan Raza Bukhari <51364060+eruditehassan@users.noreply.github.com>
2019-06-15 23:52:15 +0500

    Create AnotherPersonInterface.java

---
+++ Interfaces/PersonInterface/AnotherPersonInterface.java
@@ -0,0 +1,5 @@
+public interface AnotherPersonInterface {
+    //AnotherPersonInterface that can be implemented in classes
+    //Methods that will be implemented
+    public int measureIntelligence(String parameter);
+}
