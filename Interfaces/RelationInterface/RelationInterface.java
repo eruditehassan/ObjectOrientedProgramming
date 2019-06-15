b5f943a6c75d237e0db4d6f69d8a9171014f1c75
Hassan Raza Bukhari <51364060+eruditehassan@users.noreply.github.com>
2019-06-15 23:53:47 +0500

    Create RelationInterface.java

---
+++ Interfaces/RelationInterface/RelationInterface.java
@@ -0,0 +1,8 @@
+public interface RelationInterface {
+    //This interface will be used to compare and find relation between different objects
+    //All the necessary methods that will be implemented
+    public boolean isGreater(Object one, Object two);
+    public boolean isLess(Object one, Object two);
+    public boolean isEqual(Object one, Object two);
+    
+}
